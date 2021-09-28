3f7475110a596dbcdc999e80fafff5489cbbf3c3
Roberto-Montanez <a01253189@itesm.mx>
2021-09-28 16:38:05 +0000

    Lista 1

---
+++ assignments/01Listas/src/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    #escribe tu código abajo de esta línea
+    lista = ['Matemáticas','Física','Química','Historia','Lengua']
+    i = 0
+    n = 5
+    while i<n :
+        print('Yo estudio '+ lista[i])
+        i = i+1
+
+if __name__=='__main__':
+    main()
